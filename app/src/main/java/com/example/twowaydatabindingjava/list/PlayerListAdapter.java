311b27d20ad43391f8ebf8d00c8250cb2e8fe997
Wildan Kurniadi <danprogramming10@gmail.com>
2020-09-18 16:33:57 +0700

    #3 RecyclerViewAdapterViewHolder using dataBinding(viewBinding style)

--- app/src/main/java/com/example/twowaydatabindingjava/list/PlayerListAdapter.java
+++ app/src/main/java/com/example/twowaydatabindingjava/list/PlayerListAdapter.java
@@ -1,21 +1,19 @@
 package com.example.twowaydatabindingjava.list;
 
-import android.text.style.UpdateLayout;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.twowaydatabindingjava.R;
+import com.example.twowaydatabindingjava.databinding.ItemPlayerBinding;
 
 import java.util.List;
 
 public class PlayerListAdapter extends RecyclerView.Adapter<PlayerListAdapter.ViewHolder> {
     private List<PlayerData> players;
-
 
     public PlayerListAdapter(List<PlayerData> players) {
         this.players = players;
@@ -24,13 +22,15 @@ public class PlayerListAdapter extends RecyclerView.Adapter<PlayerListAdapter.Vi
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_player, parent, false);
-        return new ViewHolder(view);
+        ItemPlayerBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.item_player, parent, false);
+
+        return new ViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         final PlayerData playerData = players.get(position);
+
         holder.bindView(playerData);
     }
 
@@ -40,17 +40,16 @@ public class PlayerListAdapter extends RecyclerView.Adapter<PlayerListAdapter.Vi
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
-        TextView playerName;
-        TextView playerNumber;
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            playerName = itemView.findViewById(R.id.tv_name);
-            playerNumber = itemView.findViewById(R.id.tv_number);
+        ItemPlayerBinding binding;
+
+        public ViewHolder(@NonNull ItemPlayerBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
 
         void bindView(PlayerData playerData){
-            playerName.setText(playerData.getPlayerName());
-            playerNumber.setText(playerData.getPlayerNumber());
+            binding.tvName.setText(playerData.getPlayerName());
+            binding.tvNumber.setText(playerData.getPlayerNumber());
         }
     }
 }
